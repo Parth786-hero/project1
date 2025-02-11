83dd57c5f7b85d510a8f0b0c3acf890ad46bafcd
kapoor <PKAPOOR4@jaguarlandrover.com>
2025-02-11 14:00:35 +0000

    Updates

--- src/myStore.js
+++
@@ -1,9 +0,0 @@
-import {create} from 'zustand';
-const store = (set)=>({
-    open : false,
-    updateOpen : (state)=>{
-        set({open : state})
-    }
-})
-
-export const useStore = create(store);
